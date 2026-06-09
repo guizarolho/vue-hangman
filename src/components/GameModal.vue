80336010a0a3824931ac37104b7cb5e1456a64ea
guizarolho <guimfontes7@gmail.com>
2026-06-09 20:11:39 -0300

    modal component

---
+++ src/components/GameModal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/utils/consts'
+
+defineProps<{
+  show: boolean
+  victory: boolean
+  message: string
+}>()
+
+const emit = defineEmits<{
+  close: []
+  restart: []
+}>()
+</script>
+
+<template>
+  <div v-if="show" class="modal-overlay">
+    <div class="modal">
+      <h1>
+        {{ victory ? VICTORY_MESSAGE : DEFEAT_MESSAGE }}
+      </h1>
+
+      <p>{{ message }}</p>
+
+      <div class="modal__actions">
+        <button @click="emit('restart')">Jogar Novamente</button>
+
+        <button @click="emit('close')">Fechar</button>
+      </div>
+    </div>
+  </div>
+</template>
