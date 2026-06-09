eabb90b1ccf2b2699824b902d03bb8d2a699e6dc
guizarolho <guimfontes7@gmail.com>
2026-06-09 13:32:46 -0300

    keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import GameHeader from './components/GameHeader.vue'
 import GameTile from './components/GameTile.vue'
+import GameKeyboard from './components/GameKeyboard.vue'
 
 const secrets = ['test', 'queen', 'jack', 'joker']
 </script>
 
 <template>
   <GameHeader />
-  <div class="word__container">
+  <div class="word__container" style="display: flex; flex-direction: row">
     <GameTile v-for="(char, index) in [...(secrets[0] ?? '')]" :key="index" :letter="char" />
   </div>
+  <GameKeyboard />
 </template>
 
 <style scoped></style>
---
+++ src/components/GameKeyboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="keyboard__container">
+    <div id="firstLine">
+      <button type="button" value="q">q</button>
+      <button type="button" value="w">w</button>
+      <button type="button" value="e">e</button>
+      <button type="button" value="r">r</button>
+      <button type="button" value="t">t</button>
+      <button type="button" value="y">y</button>
+      <button type="button" value="u">u</button>
+      <button type="button" value="i">i</button>
+      <button type="button" value="o">o</button>
+      <button type="button" value="p">p</button>
+    </div>
+    <div id="secondLine">
+      <button type="button" value="a">a</button>
+      <button type="button" value="s">s</button>
+      <button type="button" value="d">d</button>
+      <button type="button" value="f">f</button>
+      <button type="button" value="g">g</button>
+      <button type="button" value="h">h</button>
+      <button type="button" value="j">j</button>
+      <button type="button" value="k">k</button>
+      <button type="button" value="l">l</button>
+    </div>
+    <div id="thirdLine">
+      <button type="button" value="z">z</button>
+      <button type="button" value="x">x</button>
+      <button type="button" value="c">c</button>
+      <button type="button" value="v">v</button>
+      <button type="button" value="b">b</button>
+      <button type="button" value="n">n</button>
+      <button type="button" value="m">m</button>
+    </div>
+    <div id="fourthLine">
+      <button type="button">Enter</button>
+      <button type="button">Backspace</button>
+    </div>
+  </div>
+</template>
