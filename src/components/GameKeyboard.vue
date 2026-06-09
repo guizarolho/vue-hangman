ee07eb4e3ca25ed1fd5bda783caf66f4823f136e
guizarolho <guimfontes7@gmail.com>
2026-06-09 15:23:42 -0300

    keyboard iterate

--- src/components/GameKeyboard.vue
+++ src/components/GameKeyboard.vue
@@ -1,40 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const selected = ref('')
+const keyboardRows: string[][] = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+]
+
+function selectChar(char: string) {
+  selected.value = char
+}
+</script>
+
 <template>
   <div class="keyboard__container">
-    <div class="keyboard__row" id="firstLine">
-      <button class="keyboard__button" type="button" value="q">q</button>
-      <button class="keyboard__button" type="button" value="w">w</button>
-      <button class="keyboard__button" type="button" value="e">e</button>
-      <button class="keyboard__button" type="button" value="r">r</button>
-      <button class="keyboard__button" type="button" value="t">t</button>
-      <button class="keyboard__button" type="button" value="y">y</button>
-      <button class="keyboard__button" type="button" value="u">u</button>
-      <button class="keyboard__button" type="button" value="i">i</button>
-      <button class="keyboard__button" type="button" value="o">o</button>
-      <button class="keyboard__button" type="button" value="p">p</button>
-    </div>
-    <div class="keyboard__row" id="secondLine">
-      <button class="keyboard__button" type="button" value="a">a</button>
-      <button class="keyboard__button" type="button" value="s">s</button>
-      <button class="keyboard__button" type="button" value="d">d</button>
-      <button class="keyboard__button" type="button" value="f">f</button>
-      <button class="keyboard__button" type="button" value="g">g</button>
-      <button class="keyboard__button" type="button" value="h">h</button>
-      <button class="keyboard__button" type="button" value="j">j</button>
-      <button class="keyboard__button" type="button" value="k">k</button>
-      <button class="keyboard__button" type="button" value="l">l</button>
-    </div>
-    <div class="keyboard__row" id="thirdLine">
-      <button class="keyboard__button" type="button" value="z">z</button>
-      <button class="keyboard__button" type="button" value="x">x</button>
-      <button class="keyboard__button" type="button" value="c">c</button>
-      <button class="keyboard__button" type="button" value="v">v</button>
-      <button class="keyboard__button" type="button" value="b">b</button>
-      <button class="keyboard__button" type="button" value="n">n</button>
-      <button class="keyboard__button" type="button" value="m">m</button>
-    </div>
-    <div class="keyboard__row" id="fourthLine">
-      <button class="keyboard__button" type="button">Enter</button>
-      <button class="keyboard__button" type="button">Backspace</button>
+    <div v-for="(row, index) in keyboardRows" :key="`row-${index}`" class="keyboard__row">
+      <button v-for="char in row" :key="char" @click="selectChar(char)" class="keyboard__button">
+        {{ char }}
+      </button>
     </div>
   </div>
 </template>
