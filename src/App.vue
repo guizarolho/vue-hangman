939b78c1c2fd59202111a0ee0619ca7bce51c1e1
guizarolho <guimfontes7@gmail.com>
2026-06-09 12:31:50 -0300

    game tile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GameTile from './components/GameTile.vue'
+
+const secrets = ['test', 'queen', 'jack', 'joker']
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <GameTile v-for="(char, index) in [...(secrets[0] ?? '')]" :key="index" :letter="char" />
 </template>
 
 <style scoped></style>
---
+++ src/components/GameTile.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  letter: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.letter }}</h1>
+  </div>
+</template>
